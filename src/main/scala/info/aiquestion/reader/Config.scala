dd3c52e7cd87d66188c180a7c69023a4400b7968
Shawn <wangxiaofan0529@gmail.com>
2015-05-24 20:59:37 +0800

    Add Config file

---
+++ src/main/scala/info/aiquestion/reader/Config.scala
@@ -0,0 +1,15 @@
+package info.aiquestion.reader
+
+object Config {
+
+  val KINDLESIZE = "680*960"
+
+  val PHANTOMJSBIN = ""
+  val PHANTOMJSSCRIPT = ""
+  val TEMPFILEROOT = ""
+  val RESULTROOT = ""
+  val DBPORT = 27017
+
+  val EMAIL = ""
+  val PASSWORD = ""
+}
